93d9ec5396bc9a097f0342cdb9a33794d52aec57
Jack Lee <jacklee@jacks-macbook.local>
2021-03-12 23:22:39 -0800

    Refactored setup.sh

--- setup.sh
+++ setup.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
-# Instructions
-# ============
-# Install Vim 8+ and Python 3 manually if the script does not work.
-# Delete existing dotfiles (e.g., ~/.vimrc, ~/.vim, ~/.tmux.conf).
-
 # require vim 8+ and python 3
 if [ -n "$(command -v apt-get)" ]; then
-    sudo add-apt-repository ppa:jonathonf/vim
-    sudo apt update 
-    sudo apt install vim
-    sudo apt install python3.6
+  sudo add-apt-repository ppa:jonathonf/vim
+  sudo apt update 
+  sudo apt install vim
+  sudo apt install python3.6
+else
+  echo "WARNING: Command 'apt-get' not found. Please install dependencies manally."
 fi
 
 # download vim-plug
 curl -fLo ./vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # setup symlinks
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
