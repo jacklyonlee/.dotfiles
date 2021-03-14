a6cdd4584fe91b7606353e7436a078b22e584bab
Jack Lee <jacklee@jacks-macbook.local>
2021-03-13 16:44:34 -0800

    Modified curl to be called from anywhere

--- setup.sh
+++ setup.sh
@@ -10,12 +10,14 @@ else
   echo "WARNING: Command 'apt-get' not found. Please install dependencies manually."
 fi
 
+# set current directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
 # download vim-plug
-curl -fLo ./vim/autoload/plug.vim --create-dirs \
+curl -fLo $DIR/vim/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # setup symlinks
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 ln -s $DIR/vim ~/.vim
 ln -s $DIR/vimrc ~/.vimrc
 ln -s $DIR/tmux.conf ~/.tmux.conf
